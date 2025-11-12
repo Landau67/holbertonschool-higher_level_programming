c9b0944c6b3257a28ada998385abc2ff618512bb
landau <landau@landau.landau>
2025-11-12 19:15:22 +0400

    100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-for i in range(26):
-    if i % 2 == 0:
-        print(chr(ord('z') - i), end="")
-    else:
-        print(chr(ord('Y') - (i - 1)), end="")
+print("".join(chr(ord('z') - i) if i % 2 == 0 else chr(ord('Y') - (i - 1)) for i in range(26)), end="")
