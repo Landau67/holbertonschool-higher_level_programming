17ca6f4a17dbe846897210ec4ebdbc37e82f41fd
landau <landau@landau.landau>
2025-11-17 11:52:13 +0400

    calculator100

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import sys
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
-    from calculator_1 import add, sub, mul, div
 
     a = int(sys.argv[1])
     operator = sys.argv[2]
