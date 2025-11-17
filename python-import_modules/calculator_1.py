d12a3593004f1317829653c7cc554052f0d1b1a5
landau <landau@landau.landau>
2025-11-17 11:48:49 +0400

    calculator100

--- python-import_modules/calculator_1.py
+++ python-import_modules/calculator_1.py
@@ -1,53 +1,53 @@
 #!/usr/bin/python3
+import sys
 
-if __name__ == "__main__":
-    def add(a, b):
-        """My addition function
+def add(a, b):
+    """My addition function
 
-        Args:
-            a: first integer
-            b: second integer
+    Args:
+        a: first integer
+        b: second integer
 
-        Returns:
-            The return value. a + b
-        """
-        return (a + b)
-
-
-    def sub(a, b):
-        """My subtraction function
-
-        Args:
-            a: first integer
-            b: second integer
-
-        Returns:
-            The return value. a - b
-        """
-        return (a - b)
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
 
 
-    def mul(a, b):
-        """My multiplication function
+def sub(a, b):
+    """My subtraction function
 
-        Args:
-            a: first integer
-            b: second integer
+    Args:
+        a: first integer
+        b: second integer
 
-        Returns:
-            The return value. a * b
-        """
-        return (a * b)
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
 
 
-    def div(a, b):
-        """My division function
+def mul(a, b):
+    """My multiplication function
 
-        Args:
-            a: first integer
-            b: second integer
+    Args:
+        a: first integer
+        b: second integer
 
-        Returns:
-            The return value. a / b
-        """
-        return int(a / b)
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
