0916d453eff59bf69b52524ffd6368a8e9ad35ab
landau <landau@landau.landau>
2025-11-18 00:17:46 +0400

    Only by 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     result_list = []
     for i in my_list:
         if i % 2 == 0:
-            result_list(True)
+            result_list.append(True)
         else:
-            result_list(False)
+            result_list.append(False)
     return result_list
