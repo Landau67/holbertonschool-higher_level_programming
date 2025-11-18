d87f45cdb03fdecebe57cdbf47695e43e2ce62bd
landau <landau@landau.landau>
2025-11-18 13:49:07 +0400

    Weighted average!

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    s = 0
-    b = 0
-    n = 0
-    for row in my_list:
-        s += row[0] * row[1]
-        n += row[1]
-    b = s / n
-    return b
+    if len(my_list) != 0:
+        s = 0
+        b = 0
+        n = 0
+        for row in my_list:
+            s += row[0] * row[1]
+            n += row[1]
+        b = s / n
+        return b
+    else:
+        return 0
