a61bb4f75e880a0b630fb0142401a06229d1d1c2
landau <landau@landau.landau>
2025-11-18 09:45:26 +0400

    Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix=[]
+    new_matrix = []
     for row in matrix:
-        new_row=[]
+        new_row = []
         for value in row:
             new_row.append(value ** 2)
         new_matrix.append(new_row)
