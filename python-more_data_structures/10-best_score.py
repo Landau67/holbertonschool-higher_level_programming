891092275ef4cd8ef847715515b0a6151e5081b0
landau <landau@landau.landau>
2025-11-18 11:42:21 +0400

    Best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,4 +3,10 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-        return max(a_dictionary)
+        value = 0
+        name = ""
+        for i in a_dictionary.keys():
+            if a_dictionary[i] > value:
+                value = a_dictionary[i]
+                name = i
+    return name
