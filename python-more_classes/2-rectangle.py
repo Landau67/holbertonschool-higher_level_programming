772c6e0cd9c6f19c4c2b5aeab581ef63ab1db37e
landau <landau@landau.landau>
2025-11-24 10:57:34 +0400

    Area and Perimeter

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -38,4 +38,6 @@ class Rectangle:
 
     @property
     def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
