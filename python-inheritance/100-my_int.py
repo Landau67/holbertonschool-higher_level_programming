a0287d63c1736a612850921b52ea4356fa1afaf0
landau <landau@landau.landau>
2025-11-25 21:36:31 +0400

    ...

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pyhon3
+"""nijbguii"""
+
+
+class MyInt(int):
+    """gnjfgfigbdfb"""
+
+    def __eq__(self, other):
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        return super().__eq__(other)
