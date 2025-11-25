82d060e83e5b29deaa959fca8834e617ab4ca047
landau <landau@landau.landau>
2025-11-25 19:39:29 +0400

    ...

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,10 +2,21 @@
 """ojsdfhvifdyh"""
 
 
-class MyList:
+class MyList(list):
     """fwjonfhyiyhi"""
 
     def print_sorted(self):
         """fhgvyfdhgdfgbfdb"""
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))
+
+
+
+my_list = MyList()
+my_list.append(1)
+my_list.append(4)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+print(my_list)
+my_list.print_sorted()
+print(my_list)
