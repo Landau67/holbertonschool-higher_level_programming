526c519ddfb80b1324629d38412a80719bbc291c
landau <landau@landau.landau>
2025-11-25 21:58:06 +0400

    ...

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -2,7 +2,7 @@
 """ijgfbvuidfbufdn"""
 
 
-def add_attr(obj, attr_name, attr_value):
+def add_attribute(obj, attr_name, attr_value):
     """ojsdfviudsfidfs"""
     if hasattr(obj, '__dict__'):
         setattr(obj, attr_name, attr_value)
