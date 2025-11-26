3e4cecbc4a35286dc3eb3fbbc0118fd1910f0973
landau <landau@landau.landau>
2025-11-26 10:48:11 +0400

    ...

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""bwyfgbvhibfvhhvbf"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """boidnfonvf"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.loads(f.read())
