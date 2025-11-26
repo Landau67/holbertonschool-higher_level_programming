0123a21ffce43c3e375b9f144f66dae574d34e79
landau <landau@landau.landau>
2025-11-26 12:26:45 +0400

    ...

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""vjsndjfvnofnvsfmk"""
+
+
+def pascal_triangle(n):
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                value = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                row.append(value)
+        triangle.append(row)
+    return triangle
