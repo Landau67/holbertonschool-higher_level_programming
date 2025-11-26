d2f9816eb1a72cce9818a7fd8abc44782edd8b7b
landau <landau@landau.landau>
2025-11-26 10:02:00 +0400

    ...

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,6 +4,6 @@
 
 def write_file(filename="", text=""):
     """ihbdshbvhsdbv"""
-    with open(filename, encoding="utf-8") as f:
-        read_data = f.read()
-        print(len(read_data))
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(text)
+        return len(text)
