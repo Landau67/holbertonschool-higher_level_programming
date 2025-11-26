d2f8f01b30ed29201c9e1304afec51bde933c3e3
landau <landau@landau.landau>
2025-11-26 12:05:48 +0400

    ...

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""fisduvudsnfv"""
+
+
+class Student:
+    """vsdvsdvsdvds"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            Result = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    Result[i] = getattr(self, i)
+        return Result
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -8,3 +8,4 @@ def load_from_json_file(filename):
     """boidnfonvf"""
     with open(filename, 'r', encoding="utf-8") as f:
         return json.loads(f.read())
+
