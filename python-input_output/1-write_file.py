f29119e211244752818ffdb00fdd3577c6bf36f8
landau <landau@landau.landau>
2025-11-26 09:58:23 +0400

    ...

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""fjgnidfbuf"""
+
+
+def write_file(filename="", text=""):
+    """ihbdshbvhsdbv"""
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(len(read_data))
