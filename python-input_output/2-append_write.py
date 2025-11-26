933059d0975d3c4fcc50f51eaf35815d01c19149
landau <landau@landau.landau>
2025-11-26 10:16:39 +0400

    ...

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""sddfvsdvsdv"""
+
+
+def append_write(filename="", text=""):
+    """vjnsfivf"""
+    with open(filename, 'a', encoding="utf-8") as f:
+        f.write(text)
+        return len(text)
