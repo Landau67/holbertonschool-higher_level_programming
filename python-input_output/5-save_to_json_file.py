9cd876c686a113ad818140089976ea2e3b9913bc
landau <landau@landau.landau>
2025-11-26 10:33:50 +0400

    ...

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""vjnsdjivbisdnvsdvnsdun"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w', encoding="utf-8") as f:
+        write = json.dumps(my_obj)
+        f.write(write)
