a6248032805875475981319531fbdf9b8c6433dc
landau <landau@landau.landau>
2025-11-26 10:34:11 +0400

    ...

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,6 +5,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """iuebfvuifebuv"""
     with open(filename, 'w', encoding="utf-8") as f:
         write = json.dumps(my_obj)
         f.write(write)
