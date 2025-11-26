6e26d63980e7c2f54eb9765b521d86dd781d0ba5
landau <landau@landau.landau>
2025-11-26 11:04:18 +0400

    ...

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,17 +3,19 @@
 
 import json
 import os
+import sys
 
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+filename = "add_item.json"
 
-def main():
-    """jvnsduifvuisdnkvsd"""
-    filename = "add_item.json"
-    if os.path.exists(filename):
-        my_list = load_from_json_file(filename)
-    else:
-        my_list = []
-    save_to_json_file(my_list, filename)
+if os.path.exists(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
+
+my_list.extend(sys.argv[1:])
+
+save_to_json_file(my_list, filename)
