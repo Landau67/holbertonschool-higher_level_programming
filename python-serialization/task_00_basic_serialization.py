89a881939ac59f8630ca602d94b0ff05fdde3ef6
landau <landau@landau.landau>
2025-11-27 17:34:23 +0400

    ...

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -13,4 +13,4 @@ def serialize_and_save_to_file(filename, data):
 
 def load_and_deserialize(filename):
     with open(filename, 'rb') as file:
-        pickle.load(file)
+        return pickle.load(file)
