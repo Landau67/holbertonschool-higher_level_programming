2d9101d91ce54905c84cd50fd359e22b10cd74ae
landau <landau@landau.landau>
2025-11-27 18:31:08 +0400

    ...

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""jndivnefdnvkwds"""
+import pickle
+
+
+class CustomObject:
+    """kosdnviusdfvofnmv"""
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        try:
+            with open(filename, 'wb') as file:
+                pickle.dump(self, file)
+        except Exception:
+            None
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, 'rb') as file:
+                return pickle.load(file)
+        except Exception:
+            return None
