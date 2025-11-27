753bb7caf376daa118d5cbd38bf3065a309c64b8
landau <landau@landau.landau>
2025-11-27 17:32:10 +0400

    ...

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import pickle
+
+
+def serialize_and_save_to_file(filename, data):
+    try:
+        with open(filename, 'wb') as file:
+            pickle.dump(data, file)
+    except (TypeError, pickle.PickleError, AttributeError):
+        raise TypeError("Object is not serializable")
+
+
+def load_and_deserialize(filename):
+    with open(filename, 'rb') as file:
+        pickle.load(file)
