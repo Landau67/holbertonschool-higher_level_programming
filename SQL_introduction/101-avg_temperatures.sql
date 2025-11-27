fcdd642bd3c0c32b19b08dcd0ef69f3828fa8f8e
landau <landau@landau.landau>
2025-11-28 01:04:43 +0400

    ...

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- Temperatures #0
-SELECT city, AVG(value) as avg_temp FROM temperatures ORDER BY avg_temp DESC;
+SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY avg_temp ORDER BY avg_temp DESC;
