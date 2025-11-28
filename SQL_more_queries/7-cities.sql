177c354f242a87b20e81ba376efb54272b68b263
landau <landau@landau.landau>
2025-11-28 10:45:52 +0400

    ...

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- Cities table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities(
+    id INT NOT NULL AUTO_INCREMENT;
+    name VARCHAR(256) NOT NULL;
+    state_id INT NOT NULL;
+    UNIQUE(id);
+    PRIMARY KEY(id);
+    FOREIGN KEY(state_id) REFERENCES state(id)
+);
