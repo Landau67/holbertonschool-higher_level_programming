045738df829deef612cfc240b40b8d9f7504976d
landau <landau@landau.landau>
2025-11-28 10:14:51 +0400

    ...

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- Root user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTITY BY 'password';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGIES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGIES;
