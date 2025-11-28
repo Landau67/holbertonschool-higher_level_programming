571bce35b5c09b0b904c534d46cdd4ebaff401f3
landau <landau@landau.landau>
2025-11-28 11:36:00 +0400

    ...

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Cities by States
-SELECT cities.id , cities.name, states.name
+SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = state_id
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
