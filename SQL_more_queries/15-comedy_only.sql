e6f37ba06554306e0dd9b5fc15849927b1fb38d4
landau <landau@landau.landau>
2025-11-28 20:17:34 +0400

    ...

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- Only Comedy
 SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_shows.genres ON tv_shows.id = tv_shows.genres.show.id
+FROM tv_shows t
+JOIN tv_show.genres ON t.id = tv_shows.genres.show.id
 JOIN tv_genres ON tv_genres.id = tv_show.genres.genre_id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
