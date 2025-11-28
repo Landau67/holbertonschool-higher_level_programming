0f591a83ff9919efaa43f452add7cc850630de5c
landau <landau@landau.landau>
2025-11-28 11:17:40 +0400

    ...

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- Cities of California
-SELECT * FROM cities;
-SELECT * FROM cities WHERE state_id = (
+SELECT id, name FROM cities WHERE state_id = (
     SELECT id 
     FROM states 
     WHERE name = 'California'
