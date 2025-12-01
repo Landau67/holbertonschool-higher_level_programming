66d82b2e08d4236bec091619e744acd0b9dd6fa2
landau <landau@landau.landau>
2025-12-01 10:13:24 +0400

    ...

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.Request('https://intranet.hbtn.io/status') as response:
         body = response.read()
-print("Body response:")
-print(f"    - type: {type(body)}")
-print(f"    - content: {body}")
-print(f"    - utf8 content: {body.decode('utf-8')}")
+        print("Body response:")
+        print(f"    - type: {type(body)}")
+        print(f"    - content: {body}")
+        print(f"    - utf8 content: {body.decode('utf-8')}")
