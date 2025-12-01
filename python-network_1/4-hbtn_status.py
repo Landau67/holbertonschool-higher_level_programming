fc04eaa816ffe0e38a2a53841e42c065daa0ebc4
landau <landau@landau.landau>
2025-12-01 11:21:19 +0400

    ...

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
     response = requests.get(url)
     print("Body response:")
-    print(f"\t- type:{type(response.text)}")
-    print(f"\t- content:{response.text}")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
