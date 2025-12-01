05e5a98ec1c284820ed92f0856dfcfacf5cd3450
landau <landau@landau.landau>
2025-12-01 11:06:29 +0400

    ...

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -8,10 +8,9 @@ import urllib.error
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    
     try:
         with urllib.request.urlopen(url) as response:
             body = response.read().decode('utf-8')
             print(body)
     except urllib.error.HTTPError as e:
-        raise(f"Error code :{e.code}")
+        print(f"Error code: {e.code}")
